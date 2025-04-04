92c9d3c6bf117516ff23d271b7b6bc81f280ec81
CyberHatcher <167662735+TA-wiah@users.noreply.github.com>
2025-04-04 23:10:59 +0000

    Update server.js
    ^}{^

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const webhookRoutes = require('./webhook');
 
 const app = express();
 app.use(express.json());
+
+// Serve static files from "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', webhookRoutes);
+
+// Add this route to handle callback
+app.get('/callback', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/success.html'));
+});
 
 app.listen(3000, () => console.log("Webhook server running on port 3000"));
